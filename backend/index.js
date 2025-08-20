22322b08b77e35eb98d749d84b5713d01b932aa5
Nivedita Patle <96255708+NiveditaPatle@users.noreply.github.com>
2025-08-20 10:40:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000
 
 // const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://realtime-chatapp-kufz.onrender.com",
     credentials: true 
 }))
 app.use(express.json())
